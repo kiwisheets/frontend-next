2078c7918efaa189bbb0c599a7bcdbcb47855014
Max Troughear <max@maxtroughear.dev>
2021-02-17 23:20:59 +1300

    remove loading state

--- components/Page.js
+++ components/Page.js
@@ -5,35 +5,27 @@ import AppNavigation from '@/components/AppNavigation';
 import Login from '@/components/Login';
 
 import { useIsLoggedInQuery } from '@/graphql/graphql';
-import FullPageSpinner from './FullPageSpinner';
 
 export default function Page({ noAuth, children }) {
   const { loading, data, error } = useIsLoggedInQuery();
 
   if (loading) {
+    return null;
+  }
+
+  if (noAuth || (!loading && !error && data.isLoggedIn)) {
     return (
-      <div key="spinner">
-        <FullPageSpinner />
-      </div>
+      <Fade in>
+        <>
+          <AppNavigation onLogout={() => Router.push('/login')}>
+            <Container maxWidth="xl" disableGutters>
+              {children}
+            </Container>
+          </AppNavigation>
+        </>
+      </Fade>
     );
   }
 
-  if (!noAuth && (error || !data.isLoggedIn)) {
-    // verify auth
-    // Router.push('/login');
-    // return <p>Loading page...</p>;
-    return <Login />;
-  }
-
-  return (
-    <Fade in>
-      <>
-        <AppNavigation onLogout={() => Router.push('/login')}>
-          <Container maxWidth="xl" disableGutters>
-            {children}
-          </Container>
-        </AppNavigation>
-      </>
-    </Fade>
-  );
+  return <Login />;
 }
